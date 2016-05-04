73561e2a768e5bca17b64155644422a99ae5f9e0
Matthew W <matthew0898@matthew0898.com>
2016-05-04 08:34:48 -0500

    Added files via upload

---
+++ ScheduleManager.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class ScheduleManager here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ScheduleManager
+{
+    static ScheduleGUI gui;
+    public static void main(String[] args){
+        gui.run();
+    }
+    
+}
