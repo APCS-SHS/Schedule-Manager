13efe1f4c1c15678320b59b5b850d3a33f5fba9d
Matthew0898 <Matthew0898@matthew0898.com>
2016-05-04 09:00:32 -0500

    Updated JFrame

---
+++ Schedule-Manager/Days.java
@@ -0,0 +1,15 @@
+
+/**
+ * Abstract class Days - write a description of the class here
+ * 
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public abstract class Days
+{
+    public enum DayOfWeek
+    {
+        MONDAY,
+        TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+    }
+}
