f6a9c0b1a4c7c8f669f8f37ba21ef013327b3685
Alexandra Lenox <alenox8@students.d125.org>
2016-05-09 08:58:25 -0500

    Update

--- Screen.java
+++ Screen.java
@@ -17,8 +17,18 @@ public class Screen extends JApplet
 
     public void paint(Graphics g)
     {
-        g.setColor(Color.black);
+        g.setColor(Color.white);
         g.fillRect(0, 0, 500, 500);
+        g.setColor(Color.black);
+        g.drawString("Schedule Manager", 205, 20);
+        g.drawString("Sunday", 25, 50);
+        g.drawString("Monday", 85, 50);
+        g.drawString("Tuesday", 145, 50);
+        g.drawString("Wednesday", 210, 50);
+        g.drawString("Thursday", 293, 50);
+        g.drawString("Friday", 365, 50);
+        g.drawString("Saturday", 415, 50);
+        g.drawLine(10, 60, 490, 60);
     }
 
     public void destroy()
