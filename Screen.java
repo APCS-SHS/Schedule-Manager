351d5a30470d7df64d961d26fec4ad638c140756
Alexandra Lenox <alenox8@students.d125.org>
2016-05-11 09:01:24 -0500

    Update

--- Screen.java
+++ Screen.java
@@ -17,59 +17,62 @@ public class Screen extends JApplet
 
     public void paint(Graphics g)
     {
+        int w=getWidth();
+        int h=getHeight();
+        
         g.setColor(Color.white);
-        g.fillRect(0, 0, 500, 500);
+        g.fillRect(0, 0, w, h);
         g.setColor(Color.black);
-        g.drawString("Schedule Manager", 205, 20);
+        g.drawString("Schedule Manager", w/2-45, h/2-230);
         //days
         g.setColor(ColorLibKhavkhalyuk.PINKak());
-        g.drawString("Sunday", 25, 50);
+        g.drawString("Sunday", w/2-225, h/2-200);
         g.setColor(Color.orange);
-        g.drawString("Monday", 85, 50);
+        g.drawString("Monday", w/2-165, h/2-200);
         g.setColor(Color.yellow);
-        g.drawString("Tuesday", 145, 50);
+        g.drawString("Tuesday", w/2-105, h/2-200);
         g.setColor(Color.green);
-        g.drawString("Wednesday", 210, 50);
+        g.drawString("Wednesday", w/2-40, h/2-200);
         g.setColor(Color.blue);
-        g.drawString("Thursday", 293, 50);
+        g.drawString("Thursday", w/2+43, h/2-200);
         g.setColor(ColorLibKhavkhalyuk.SKYBLUEak());
-        g.drawString("Friday", 365, 50);
+        g.drawString("Friday", w/2+115, h/2-200);
         g.setColor(ColorLibKhavkhalyuk.PURPLEak());
-        g.drawString("Saturday", 415, 50);
+        g.drawString("Saturday", w/2+165, h/2-200);
         //lines
         g.setColor(Color.black);
-        g.drawLine(10, 60, 490, 60);
-        g.drawLine(78, 42, 78, 500);
-        g.drawLine(140, 42, 140, 500);
-        g.drawLine(203, 42, 203, 500);
-        g.drawLine(287, 42, 287, 500);
-        g.drawLine(358, 42, 358, 500);
-        g.drawLine(410, 42, 410, 500);
+        g.drawLine(w/2-240, h/2-190, w/2+240, h/2-190);
+        g.drawLine(w/2-172, h/2-208, w/2-172, h/2+250);
+        g.drawLine(w/2-110, h/2-208, w/2-110, h/2+250);
+        g.drawLine(w/2-47, h/2-208, w/2-47, h/2+250);
+        g.drawLine(w/2+37, h/2-208, w/2+37, h/2+250);
+        g.drawLine(w/2+108, h/2-208, w/2+108, h/2+250);
+        g.drawLine(w/2+160, h/2-208, w/2+160, h/2+250);
         //numbers
-        g.drawString("1", 3, 75);
-        g.drawString("2", 3, 92);
-        g.drawString("3", 3, 109);
-        g.drawString("4", 3, 126);
-        g.drawString("5", 3, 143);
-        g.drawString("6", 3, 160);
-        g.drawString("7", 3, 177);
-        g.drawString("8", 3, 194);
-        g.drawString("9", 3, 211);
-        g.drawString("10", 3, 228);
-        g.drawString("11", 3, 245);
-        g.drawString("12", 3, 262);
-        g.drawString("13", 3, 279);
-        g.drawString("14", 3, 296);
-        g.drawString("15", 3, 313);
-        g.drawString("16", 3, 330);
-        g.drawString("17", 3, 347);
-        g.drawString("18", 3, 364);
-        g.drawString("19", 3, 381);
-        g.drawString("20", 3, 398);
-        g.drawString("21", 3, 415);
-        g.drawString("22", 3, 432);
-        g.drawString("23", 3, 449);
-        g.drawString("24", 3, 466);
+        g.drawString("1", w/2-248, h/2-173);
+        g.drawString("2", w/2-248, h/2-155);
+        g.drawString("3", w/2-248, h/2-137);
+        g.drawString("4", w/2-248, h/2-119);
+        g.drawString("5", w/2-248, h/2-101);
+        g.drawString("6", w/2-248, h/2-83);
+        g.drawString("7", w/2-248, h/2-65);
+        g.drawString("8", w/2-248, h/2-47);
+        g.drawString("9", w/2-248, h/2-29);
+        g.drawString("10", w/2-252, h/2-11);
+        g.drawString("11", w/2-252, h/2+7);
+        g.drawString("12", w/2-252, h/2+25);
+        g.drawString("13", w/2-252, h/2+43);
+        g.drawString("14", w/2-252, h/2+61);
+        g.drawString("15", w/2-252, h/2+79);
+        g.drawString("16", w/2-252, h/2+97);
+        g.drawString("17", w/2-252, h/2+115);
+        g.drawString("18", w/2-252, h/2+133);
+        g.drawString("19", w/2-252, h/2+151);
+        g.drawString("20", w/2-252, h/2+169);
+        g.drawString("21", w/2-252, h/2+187);
+        g.drawString("22", w/2-252, h/2+205);
+        g.drawString("23", w/2-252, h/2+223);
+        g.drawString("24", w/2-252, h/2+241);
 
     }
 
@@ -80,7 +83,7 @@ public class Screen extends JApplet
     public void busySquare(Graphics g)
     {
         g.setColor(Color.red);
-        g.fillRect(250, 250, 55, 30);
+        g.fillRect(10, 65, 50, 15);
     }
 }
 
